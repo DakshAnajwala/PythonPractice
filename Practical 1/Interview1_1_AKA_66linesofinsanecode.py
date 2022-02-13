af068066a8fe1c1b14b28674faec287813d46c42
DakshAnajwala <70829109+DakshAnajwala@users.noreply.github.com>
2022-02-13 12:01:00 +0800

    Did a very tough python sum

--- Practical 1/Interview1_1_AKA_66linesofinsanecode.py
+++ Practical 1/Interview1_1_AKA_66linesofinsanecode.py
@@ -1,16 +1,4 @@
 # for each query,count how many times each string is occurring in this list
-def occurrences(queries):
-    repetitions = {}
-    for s in queries:
-        s = s.lower()
-        if s not in repetitions:
-            repetitions[s] = 1
-        else:
-            repetitions[s] = repetitions[s] + 1
-
-    return repetitions
-
-
 queries_test = [
     'starbucks cupertino',
     'McDonalds',
@@ -49,5 +37,30 @@ queries = [
     'cupertino ca',
 ]
 
+
+def occurrences(queries):
+    repetitions = {}
+    for s in queries:
+        s = s.lower()
+        if s not in repetitions:
+            repetitions[s] = 1
+        else:
+            repetitions[s] = repetitions[s] + 1
+    #         print(f"{s} is not a unique string")
+    # print("The others are unique strings")
+    return repetitions
+
+
+def find_unique(repetitions):
+    unique_lst = []
+    for k in repetitions:
+        if repetitions[k] == 1:
+            unique_lst.append(k)
+
+    return unique_lst
+
+
 result = occurrences(queries=queries)
 print(result)
+unique_str = find_unique(repetitions=result)
+print(f"The unique strings are :{unique_str}")
